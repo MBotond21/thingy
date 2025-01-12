23a04b88b5344d03a3294490a4bd16b5e292ba10
MBotond21 <m.botond07@gmail.com>
2025-01-12 15:41:49 +0100

    froms are a bit more responsive

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,15 +5,15 @@ export default function Login() {
 
     return <>
         <div className="formPage">
-            <form className="w-full h-full mt-40 ml-10">
+            <form className="w-full h-full mt-40">
                 <div className="formElement">
-                    <input type="email" name="email" placeholder="Email" className="rounded-lg p-2 w-4/5 border-2 outline-none transition-all bg-333 border-white focus:border-yellow-400 mb-4" required />
+                    <input type="email" name="email" placeholder="Email" className="rounded-lg p-2 w-4/5 ml-1/10 border-2 outline-none transition-all bg-333 border-white focus:border-yellow-400 mb-4" required />
                 </div>
                 <div className="formElement">
-                    <input type="password" name="psw" placeholder="Password" className="rounded-lg p-2 w-4/5 border-2 outline-none transition-all bg-333 border-white focus:border-yellow-400 mb-4" required />
+                    <input type="password" name="psw" placeholder="Password" className="rounded-lg p-2 w-4/5 ml-1/10 border-2 outline-none transition-all bg-333 border-white focus:border-yellow-400 mb-4" required />
                 </div>
-                <button className="btn w-60 ml-14" >Login</button>
-                <p className="ml-18 mt-1 text-mute">Don't have an account yet? <a onClick={() => navigate("/signup")} className="text-blue-500 hover:cursor-pointer ">Sign up</a></p>
+                <button className="btn w-1/4 ml-f-1/4" >Login</button>
+                <p className="mt-1 text-mute w-full text-center">Don't have an account yet? <a onClick={() => navigate("/signup")} className="text-blue-500 hover:cursor-pointer ">Sign up</a></p>
             </form>
         </div>
     </>
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,17 +1,17 @@
 export default function Signup(){
     return <>
         <div className="formPage">
-            <form className="w-full h-full mt-40 ml-10">
+            <form className="w-full h-full mt-40">
                 <div className="formElement">
-                    <input type="email" name="email" placeholder="Email" className="rounded-lg p-2 w-4/5 border-2 outline-none transition-all bg-333 border-white focus:border-yellow-400 mb-4" required />
+                    <input type="email" name="email" placeholder="Email" className="rounded-lg p-2 w-4/5 ml-1/10 border-2 outline-none transition-all bg-333 border-white focus:border-yellow-400 mb-4" required />
                 </div>
                 <div className="formElement">
-                    <input type="text" name="uname" placeholder="Username" className="rounded-lg p-2 w-4/5 border-2 outline-none transition-all bg-333 border-white focus:border-yellow-400 mb-4" required />
+                    <input type="text" name="uname" placeholder="Username" className="rounded-lg p-2 w-4/5 ml-1/10 border-2 outline-none transition-all bg-333 border-white focus:border-yellow-400 mb-4" required />
                 </div>
                 <div className="formElement">
-                    <input type="password" name="psw" placeholder="Password" className="rounded-lg p-2 w-4/5 border-2 outline-none transition-all bg-333 border-white focus:border-yellow-400 mb-4" required />
+                    <input type="password" name="psw" placeholder="Password" className="rounded-lg p-2 w-4/5 ml-1/10 border-2 outline-none transition-all bg-333 border-white focus:border-yellow-400 mb-4" required />
                 </div>
-                <button className="btn w-60 ml-14" >Sign up</button>
+                <button className="btn w-1/4 ml-f-1/4" >Sign up</button>
             </form>
         </div>
     </>
