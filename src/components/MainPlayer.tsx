e22483862515a1210f65e478da54bac9bf6b47b6
MBotond21 <m.botond07@gmail.com>
2025-01-13 13:12:41 +0100

    showing and playing fetched track

--- src/components/MainPlayer.tsx
+++ src/components/MainPlayer.tsx
@@ -1,8 +1,28 @@
+import { useContext, useEffect, useRef } from "react";
+import { TrackContext } from "../contexts/MusicContext";
 
-export default function MainPlayer(){
+export default function MainPlayer() {
+    const { track, loadTrack } = useContext(TrackContext);
+    const audioRef = useRef(null);
+
+    useEffect(() => {
+        loadTrack("1848357");
+    }, []);
+
+    useEffect(() => {
+        if (audioRef.current) {
+            audioRef.current.load();
+        }
+    }, [track?.audio]);
+
     return <>
         <div className="content">
-            <p>MainPlayer</p>
+            <img src={track?.image} alt="trackPic" className="hover:cursor-default track" />
+            <div className="w-full m-4 inline">
+                <audio ref={audioRef} controls className="w-3/4 ml-f-3/4 hover:cursor-default">
+                    <source src={track?.audio} type="audio/mpeg" />
+                </audio>
+            </div>
         </div>
     </>
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,15 @@ import Header from "../components/Header";
 import Main from "../components/Main";
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TrackContextProvider } from '../contexts/MusicContext';
 
 export default function Home() {
     return <>
         <Header />
-        <DndProvider backend={HTML5Backend}>
-            <Main />
-        </DndProvider>
+        <TrackContextProvider>
+            <DndProvider backend={HTML5Backend}>
+                <Main />
+            </DndProvider>
+        </TrackContextProvider>
     </>
 }
